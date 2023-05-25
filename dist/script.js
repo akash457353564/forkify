991b607645decbc46b4a82ffc98cc6b58e55d7bc
akash457353564 <102581699+akash457353564@users.noreply.github.com>
2023-05-25 14:02:45 +0530

    commit

--- dist/script.js
+++ dist/script.js
@@ -557,6 +557,18 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"6rimH":[function(require,module,exports) {
+//('TEST')
+const get_recipe = async function(url) {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message}`);
+        console.log(res, data);
+    } catch (err) {
+        console.error(err);
+    }
+};
+get_recipe(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886fergrg`);
 
 },{}]},["dpgAG","6rimH"], "6rimH", "parcelRequire3a11")
 
--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+//('TEST')
 
+
+const get_recipe = async function (url) {
+    try {
+
+        const res = await fetch(url)
+        const data = await res.json()
+
+        if(!res.ok){
+            throw new Error (`${data.message}`)
+        }
+        console.log(res, data)
+
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+
+get_recipe(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886fergrg`)
 
