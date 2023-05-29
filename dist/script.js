03e42deb40ccfae9648fd8e716c3a76ec6fb0f6e
akash457353564 <102581699+akash457353564@users.noreply.github.com>
2023-05-29 17:53:57 +0530

    commit

--- dist/script.js
+++ dist/script.js
@@ -563,7 +563,13 @@ const get_recipe = async function(url) {
         const res = await fetch(url);
         const data = await res.json();
         if (!res.ok) throw new Error(`HERE ${data.message}`);
-        console.log(res, data);
+        //console.log( data.data.recipe.ingredients)
+        const ingridients = data.data.recipe.ingredients;
+        //console.log(ingridients)
+        ingridients.forEach(function(el) {
+            //console.log(el)
+            console.log(el.description, el.quantity, el.unit);
+        });
     } catch (err) {
         console.error(err);
     }
